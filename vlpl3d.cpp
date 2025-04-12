1e3f5124a81535837b01dd56de38a68b07a5ce4d
Alexey Snytnikov <snytav@gmail.com>
2025-04-12 20:52:51 +0200

    rm commented MPI staff

--- vlpl3d.cpp
+++ vlpl3d.cpp
@@ -9,7 +9,7 @@
 
 //#include "vlpl3d.h"
 
-// #include "para.h"
+
 //#include "CUDA_WRAP/plasma_particles.h"
 
 using namespace std;
@@ -41,46 +41,6 @@ int main(int argc, char** argv)
 
 infile = "v.ini";
 
-/*  
-#ifdef V_MPI
-    if(MPI_ERRORS_RETURN == MPI_Init(&argc,&argv))
-    {
-    	char * error = "MPI_ERRORS_RETURN";
-    	printf("%s",error);
-    	exit MPI_ERRORS_RETURN;
-    };
-    int buf_size = 100000000 + MPI_BSEND_OVERHEAD;
-    char *p_b = new char[buf_size];
-    int err_buf_att = MPI_Buffer_attach( p_b, buf_size);
- 
-    MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
-    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-    MPI_Get_processor_name(processor_name,&namelen);
-    fprintf(stderr,"Process %d on %s\n",
-	    rank, processor_name);
-#endif
-*/
-/*  
-  srand( time(NULL) + rank );
-  Domain *domain = new Domain(infile, rank);
-
-  //create_h_plasma_particles();
-
-#ifdef V_MPI
-  //  MPI_Buffer_detach(p_b, &buf_size);
-  //  delete[] p_b;
-#endif
-
-#ifdef X_ACCESS
-  Plot *interactive = domain->plot;
-  if (interactive->nview) {
-    interactive->Run();
-  }
-  else {
-    domain->Run();
-  }
-#endif
-*/
 
 //printf("PARA init rank %d l_Mx %d \n",GetRank(),l_Mx);
 
